64d989704a19001d73eee5fd89e3c5fe100a5412
om3ga <om3ga6400gd@gmail.com>
2024-11-07 16:21:06 +0000

    better updater

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
 rm -rf cookies kittens lib decoder.html index.html license.txt
 
-wget -mkEpnpH https://coderpatsy.bitbucket.io/
+if wget -mkEpnpH https://coderpatsy.bitbucket.io; then
+    echo "Download done"
+else
+    echo "Download failed"
+fi
